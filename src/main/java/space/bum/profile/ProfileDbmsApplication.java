604922bc5fa59aae704bba851f4f585bbcce3d0a
osparking <jbpark03@gmail.com>
2024-06-13 08:15:55 +0900

    프로필 사용법 학습

---
+++ src/main/java/space/bum/profile/ProfileDbmsApplication.java
@@ -0,0 +1,13 @@
+package space.bum.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileDbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileDbmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/profile/ProfileDbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileDbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
